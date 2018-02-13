925e6157a3989fa775eecad64d5e485592a6d084
Cloud User <ec2-user@ip-10-0-0-29.us-west-2.compute.internal>
2018-02-13 07:16:50 +0000

    fixf fs

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,9 @@ public class Greeter {
 
   }
 
-/** @param tag forraj thias is ok
- * @return kff
+/** 
+ * @param someone the name of a person
+ * @return greeting string
  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
