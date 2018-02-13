c4fed2f5dafc9b342e1730f569fef260b8f95a3d
Cloud User <ec2-user@ip-10-0-0-29.us-west-2.compute.internal>
2018-02-13 06:42:59 +0000

    fix checkstle warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
+/** @raj thias is ok
+ * @nakd kff
+ */
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
